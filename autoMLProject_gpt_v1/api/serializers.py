7f6b2613d278b9e19771a52cdc85c1d048ad42e3
caglarsubas <caglarsubas@gmail.com>
2024-08-27 08:47:00 +0300

    old project folders are deleted

--- autoMLProject_gpt_v1/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import IrisData
-
-class IrisDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = IrisData
-        fields = '__all__'
--- automl_platform_gpt_v2/data_collection/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import iris_data
-
-urlpatterns = [
-    path('api/iris_data', iris_data, name='iris_data'),
-]
