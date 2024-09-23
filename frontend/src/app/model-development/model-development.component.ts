aef006e2988ad80866f984258562b03bcf6c0998
caglarsubas <caglarsubas@gmail.com>
2024-09-23 14:22:23 +0300

    hovering navigation menu for model-development is added.

--- frontend/src/app/model-development/model-development.component.ts
+++ frontend/src/app/model-development/model-development.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-model-development',
   templateUrl: './model-development.component.html',
-  styleUrl: './model-development.component.css'
+  styleUrls: ['./model-development.component.css']
 })
-export class ModelDevelopmentComponent {
+export class ModelDevelopmentComponent implements OnInit {
+  currentRoute: string = '';
+  menuItems = ['declaration', 'preprocessing', 'modeling', 'evaluation', 'deployment'];
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.currentRoute = this.router.url.split('/').pop() || 'declaration';
+    });
+  }
 }
